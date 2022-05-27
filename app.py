f0f8e088641d8ff0a3bb7fda8c068cfb12575471
claudio casa <pokety@gmail.com>
2022-05-27 20:39:28 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0',debug=True)
+
+
---
+++ static/script.js
@@ -0,0 +1,9 @@
+const  init=()=>{
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const inputSubmit = document.querySelector('.login_submit');
+
+    console.log(inputEmail,inputPassword,inputSubmit)
+}
+window.onload= init()
+
